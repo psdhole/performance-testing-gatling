e2ae36767b6d1b243e4ce09a4daebbeb250d23e5
Pushparaj Dhole <pushparaj.dhole23@gmail.com>
2020-05-29 18:09:28 +0200

    added simulation for get api

--- README.md
+++ README.md
@@ -11,3 +11,28 @@
     ``
     mvn clean compile gatling:test -Dgatling.simulationClass=com.isom.service.test.ISOMGetLoadTest
     ``
+
+#### Run MySQL instance locally
+
+- Use below commands to create MySQL instance , required database and table.
+  
+  
+        #Run docker container
+        docker run -d --name mysql -e MYSQL_ROOT_HOST=%  -e  MYSQL_ROOT_PASSWORD=root -v mysql-db:/var/lib/mysql -p 3306:3306 -d mysql/mysql-server	
+    
+        #Connect to  the container
+        docker exec -it mysql mysql -u root -p
+        
+        #Create required database 
+        create database isom_Service;
+        use isom_Service;
+        
+        #create table
+        CREATE TABLE  id_table (fo_id varchar(100));
+        
+        #insert sample record
+        INSERT INTO id_table (fo_id) VALUES ('1c238d20-a0b8-11ea-aafa-4de03f00b166');
+        
+        #Get records from the table
+        select * from id_table; 
+        ``
--- src/test/scala/com/isom/service/test/ISOMGetLoadTest.scala
+++ src/test/scala/com/isom/service/test/ISOMGetLoadTest.scala
@@ -1,24 +1,19 @@
 package com.isom.service.test
 
 import com.typesafe.config._
-import io.gatling.core.Predef._
-import io.gatling.core.structure.ScenarioBuilder
-import io.gatling.http.Predef.{http, _}
+import io.gatling.core.Predef.{Simulation, _}
+import io.gatling.http.Predef.{HttpHeaderValues, http}
 import io.gatling.http.protocol.HttpProtocolBuilder
-import io.gatling.http.request.builder.HttpRequestBuilder.toActionBuilder
-
-import scala.concurrent.duration._
+import io.gatling.jdbc.Predef.jdbcFeeder
 
 class ISOMGetLoadTest extends Simulation {
-
-
   val conf = ConfigFactory.load()
-  val baseUrl = conf.getString("baseUrl")
-  val apiUri = conf.getString("apiUri")
-  val throughput = conf.getInt("throughputValue")
-  val users = conf.getInt("s1-users")
-  val duration = conf.getInt("s1-duration")
-
+  val baseUrl = conf.getString("isom-baseUrl")
+  val apiUri = conf.getString("isom-apiUri")
+  val throughput = conf.getInt("isom-throughputValue")
+  val dbUrl = conf.getString("isom-dbUrl")
+  val dbUsername = conf.getString("isom-dbUserName")
+  val dbPassword = conf.getString("isom-dbPassword")
 
   val httpProtocol: HttpProtocolBuilder = http.baseUrl(baseUrl)
 
@@ -26,21 +21,16 @@ class ISOMGetLoadTest extends Simulation {
     "Accept" -> HttpHeaderValues.ApplicationJson
   )
 
-  val baseScenario1: ScenarioBuilder = scenario("isom-create-request-1")
+  val baseScenario1 = scenario("isom-get-request-1")
+    .feed(jdbcFeeder(dbUrl, dbUsername, dbPassword, "SELECT fo_id FROM id_table").circular)
     .exec(
-      http("isom-create-request-1")
-        .post(baseUrl + apiUri)
-        .body(RawFileBody("isom-create-scenario1.json")).asJson
-        .headers(headers)
-        .check(status.is(200))
+      http("isom-get-request-1")
+        .get(baseUrl + apiUri + "/${fo_id}")
     )
 
-
-  //This simulation will reach 100 req/s with a ramp of 10 seconds, then hold this throughput for 120 seconds, jump to 50 req/s and finally hold this throughput for 60 seconds.
-
   setUp(
-    baseScenario1.inject(constantUsersPerSec(100) during (10 seconds)).throttle( //adds 100 users every second , so a total of 3600 users after 120 seconds
-      reachRps(10) in (10 seconds), //reachRps(target) in (duration): target a throughput with a ramp over a given duration.
-      holdFor(60 seconds) //holdFor(duration): hold the current throughput for a given duration.
-    )).protocols(httpProtocol).assertions(global.successfulRequests.percent.is(throughput))
+    baseScenario1.inject(constantUsersPerSec(10) during (10))
+  ).protocols(httpProtocol)
+    .assertions(global.successfulRequests.percent.is(throughput))
+    .assertions(global.responseTime.max.lt(2000))
 }
